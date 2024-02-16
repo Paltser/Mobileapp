0a84fc49fa39f18e1e06443aeac731864bf13e77
Paltser <Markos.Paltser@voco.ee>
2024-02-16 08:45:16 +0200

    Create authorization screens

---
+++ src/screens/auth/Signin/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "../../../utils/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  Button: {
+    marginVertical: 20,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.blue,
+    marginBottom: 56,
+  },
+  footerLink: {
+    fontWeight: "bold",
+  },
+});
